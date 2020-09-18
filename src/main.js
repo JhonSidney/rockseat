92642818e4358b92f554dc93914a151593a0a097
Jhon Sidney <jhon.sidney@webtechbr.com>
2020-09-18 11:18:04 -0300

    sync_await

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
+import { resolve } from 'bluebird';
 import  * as ClasseUsuario  from './functions';
 import {idade as idadeUsuario} from './functions';
 
 console.log(ClasseUsuario.default.info());
 console.log(idadeUsuario);
+
+
+console.log('-------------------------------');
+
+const delay = (time) => new Promise( resolve => {setTimeout( () => {resolve(time)}, 1000)});
+
+async function umporSegundo(){
+
+    console.log(await delay(1));
+    console.log(await delay(2));
+    console.log(await delay(3));
+}
+
+
+umporSegundo();
